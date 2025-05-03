35178a70d01f2029c87082d25f4c7b10346710cd
ryanG644 <55308955+ryanG644@users.noreply.github.com>
2025-05-03 17:32:30 -0400

    Delete pages directory

--- pages/index.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: '4rem', fontFamily: 'sans-serif' }}>
-      <h1 style={{ fontSize: '2.5rem' }}>Ryan Gosha</h1>
-      <p>Finance & Strategy | MBA | CFA | CIMA</p>
-      <p>Welcome to my personal site.</p>
-    </main>
-  );
-}
